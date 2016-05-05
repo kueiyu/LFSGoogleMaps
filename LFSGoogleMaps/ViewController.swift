18f827bd1657826d218c142b0f31c1dee20d232c
Dennis Lan <kyocartoon@gmail.com>
2016-05-06 00:09:51 +0800

    1.0.1(1.0.0.4) - Remove GoogleMaps Tutorial - Add taking Photo & Select Photo from photo Library - Correct Revision would be next commit

--- LFSGoogleMaps/ViewController.swift
+++ LFSGoogleMaps/ViewController.swift
@@ -9,22 +9,50 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func takePhoto(sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .Camera
+        
+        [self .presentViewController(picker, animated: true, completion: nil)]
+        
+    }
+    
+    @IBAction func selectPhoto(sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .PhotoLibrary
+        
+        [self .presentViewController(picker, animated: true, completion: nil)]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-                                                          longitude: 151.20, zoom: 6)
-        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
-        self.view = mapView
         
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+        if ([UIImagePickerController.isSourceTypeAvailable(.Camera)] != true) {
+            let myAlertView = UIAlertController(title: "Error", message: "Device has no Camera", preferredStyle: .ActionSheet)
+            
+            myAlertView.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+                
+                self.presentViewController(myAlertView, animated: true, completion: nil)
+        }
+        // Google API Tutorial
+//        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+//                                                          longitude: 151.20, zoom: 6)
+//        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+//        mapView.myLocationEnabled = true
+//        self.view = mapView
+//        
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,5 +61,15 @@ class ViewController: UIViewController {
     }
 
 
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let chosenImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        self.imageView.image = chosenImage
+        
+        [picker .dismissViewControllerAnimated(true, completion: nil)]
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        [picker .dismissViewControllerAnimated(true, completion: nil)]
+    }
 }
 
